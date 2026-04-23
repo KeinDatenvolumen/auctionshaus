325cda83beb4b3f17e6877a44e0be466c4fc7360
Enrico <54184139+KeinDatenvolumen@users.noreply.github.com>
2026-04-23 13:09:35 +0200

    Launcher Fix

---
+++ src/main/java/org/auctionsproject/ui/Launcher.java
@@ -0,0 +1,8 @@
+package org.auctionsproject.ui;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Dies ruft die main-Methode deiner App-Klasse auf
+        App.main(args);
+    }
+}
